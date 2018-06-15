781a6b406d6f94f27065c6ab9709ffc1f157913b
Usagino <yuki.itoh@front-artgraph.com>
2018-06-15 16:04:28 +0900

    無駄なコードを削除

--- script/script.js
+++ script/script.js
@@ -23,21 +23,3 @@ $(function(){
       }
    })
 })
-
-
-$(window).scroll(function (){
-	$(".contents_1").each(function(){
-	  var position = $(this).offset().top; //ページの一番上から要素までの距離を取得
-	  var scroll = $(window).scrollTop(); //スクロールの位置を取得
-	  var windowHeight = $(window).height(); //ウインドウの高さを取得
-
-	  if (scroll > position - windowHeight){ //スクロール位置が要素の位置を過ぎたとき
-	    $(this).addClass('active'); //クラス「active」を与える
-
-	  }
-    else {
-      $(this).removeClass('active');
-      console.log("false");
-    }
-	});
-});

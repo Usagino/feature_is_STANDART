dc493aec23b0e166e5e081556d34519036749cdb
Usagino <yuki.itoh@front-artgraph.com>
2018-06-29 01:13:33 +0900

    完成 ファーストビューの背景画像を変えた

--- script/script.js
+++ script/script.js
@@ -1,27 +1,3 @@
-// $(function(){
-//   var target = ".contents_1";
-//   var seperated =".headerwrap"
-//   var obj_t_pos = $(target).offset().top;
-//
-//    $(window).scroll(function(){ // スクロール毎にイベントが発火します。
-//       var scr_count = jQuery(document).scrollTop();
-//       if (scr_count  > obj_t_pos) {
-//         $(".navigation a").css("color","black");
-//
-//         $("header").css({
-//           "background-color":"white",
-//           "box-shadow": "0px 4px 10px 0px rgba(166,166,166,1)"
-//         });
-//       }else {
-//         $(".navigation a").css("color","white");
-//
-//         $("header").css({
-//           "background-color":"transparent",
-//           "box-shadow":"none"
-//         });
-//       }
-//    })
-// })
 
 
 function init() {
@@ -33,6 +9,7 @@ function init() {
         // 変化するポイントまでスクロールしたらクラスを追加
         if ( $(window).scrollTop() > px_change ) {
             $("header").addClass("header_change");
+
             console.log("up");
 
         // 変化するポイント以前であればクラスを削除

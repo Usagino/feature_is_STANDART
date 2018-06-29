a1b0be41eecc37e8a6390b83134638604878b6ca
Usagino <yuki.itoh@front-artgraph.com>
2018-06-29 12:48:23 +0900

    フッターにリンクを追加

--- script/script.js
+++ script/script.js
@@ -2,21 +2,31 @@
 
 function init() {
     // スクロールして何ピクセルでアニメーションさせるか
-    var px_change   = 300;
 
+    var px_change = 300;
+    var book_count = 5;
+    var hide = 0.3;
+    var delay = 4;
+
+
+    // window.scroll(function() {
+    //
+    // });
     // スクロールのイベントハンドラを登録
-    window.addEventListener('scroll', function(e){
-        // 変化するポイントまでスクロールしたらクラスを追加
-        if ( $(window).scrollTop() > px_change ) {
-            $("header").addClass("header_change");
+    window.addEventListener('scroll', function(){
+      var y = window.pageYOffset
+      console.log(y);
 
-            console.log("up");
 
-        // 変化するポイント以前であればクラスを削除
-        } else if ( $("header").hasClass("header_change") ) {
-            $("header").removeClass("header_change");
-            console.log("down");
-        }
+
+      // 変化するポイントまでスクロールしたらクラスを追加
+      if ( $(window).scrollTop() > px_change ) {
+          $("header").addClass("header_change");
+      // 変化するポイント以前であればクラスを削除
+      } else if ( $("header").hasClass("header_change") ) {
+          $("header").removeClass("header_change");
+      }
+
     });
 }
 window.onload = init();
